34b8a22b0643265e05c9893b16f58b803e893b45
Nilesh Mahajan <nileshmahajan49@gmail.com>
2022-07-10 18:06:39 +0530

    Fixed bug and changed the sequence of operation to collinearity check and correlation

--- AutoFeatureSelectionUsingCorrelation/AutoFeatureSelection.py
+++ AutoFeatureSelectionUsingCorrelation/AutoFeatureSelection.py
@@ -1,4 +1,6 @@
-def findBestFeatues(data, target_feature, collinear_threshold=0.80, corr_threshold=None):
+import json
+
+def findBestFeatues(data, target_feature, collinear_threshold=None, corr_threshold=None):
     '''
     Input parameter:
     data : DataFrame : Data on which want to perform feature selection
@@ -33,7 +35,42 @@ def findBestFeatues(data, target_feature, collinear_threshold=0.80, corr_thresho
         if feature != target_feature:
             del corr_obj[feature][target_feature];
             
-            
+     
+    # Remove all features those having more than threshold collinearity
+    if collinear_threshold != None:
+        i = 0;
+        while i < size_feature:
+            j = i + 1
+            dropFeature = set() # Want to keep unique values
+            feature1 = available_features[i]
+            feature1_removed = False # check whether feture is getting removed or not
+
+            while j < size_feature:
+                feature2 = available_features[j]
+                if abs(corr_obj[feature1][feature2]) >= collinear_threshold:
+                    corr_of_features = abs(corr_obj[feature1][feature2]);
+                    corr_feature1_with_target = corr_obj[target_feature][feature1]
+                    corr_feature2_with_target = corr_obj[target_feature][feature2]
+
+                    if corr_obj[target_feature][feature1] >= corr_obj[target_feature][feature2]:
+                        dropFeature.add(feature2);
+                        reduced_features[feature2] = explaination_collinear.format(feature1, feature2, corr_of_features, collinear_threshold, target_feature, corr_feature1_with_target, corr_feature2_with_target);
+                    else:
+                        dropFeature.add(feature1);
+                        reduced_features[feature1] = explaination_collinear.format(feature2, feature1, corr_of_features, collinear_threshold, target_feature, corr_feature2_with_target, corr_feature1_with_target);
+                        feature1_removed = True;
+                j = j + 1;
+
+            for feature1  in dropFeature:
+                size_feature = size_feature - 1
+                available_features.remove(feature1);
+                del corr_obj[feature1];
+                for feature2 in corr_obj.keys():
+                    del corr_obj[feature2][feature1];
+
+            if feature1_removed == False:
+                i = i + 1;
+     
     # Remove all features those having less correlation with target variable than correlation threshold
     if corr_threshold != None:
         for feature in corr_obj[target_feature].keys():
@@ -47,39 +84,7 @@ def findBestFeatues(data, target_feature, collinear_threshold=0.80, corr_thresho
             available_features.remove(feature);
             size_feature = size_feature - 1;
             
-    # Remove all features those having more than threshold collinearity
-    while i < size_feature:
-        j = i + 1
-        dropFeature = set() # Want to keep unique values
-        feature1 = available_features[i]
-        feature1_removed = False # check whether feture is getting removed or not
-        
-        while j < size_feature:
-            feature2 = available_features[j]
-            if abs(corr_obj[feature1][feature2]) >= collinear_threshold:
-                corr_of_features = abs(corr_obj[feature1][feature2]);
-                corr_feature1_with_target = corr_obj[target_feature][feature1]
-                corr_feature2_with_target = corr_obj[target_feature][feature2]
-                                
-                if corr_obj[target_feature][feature1] >= corr_obj[target_feature][feature2]:
-                    dropFeature.add(feature2);
-                    reduced_features[feature2] = explaination_collinear.format(feature1, feature2, corr_of_features, collinear_threshold, target_feature, corr_feature1_with_target, corr_feature2_with_target);
-                else:
-                    dropFeature.add(feature1);
-                    reduced_features[feature1] = explaination_collinear.format(feature2, feature1, corr_of_features, collinear_threshold, target_feature, corr_feature2_with_target, corr_feature1_with_target);
-                    feature1_removed = True;
-            j = j + 1;
-        
-        for feature1  in dropFeature:
-            available_features.remove(feature1);
-            del corr_obj[feature1];
-            for feature2 in corr_obj.keys():
-                del corr_obj[feature2][feature1];
-        
-        if feature1_removed == True:
-            size_feature = size_feature - 1;
-        else:
-            i = i + 1;
+    
     corr_with_target = dict()
     
     for feature1, corr1 in corr_obj[target_feature].items():
